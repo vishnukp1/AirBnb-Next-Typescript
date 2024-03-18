1fa59407201a6abfc87fad28c83d5d4d3e16c4bd
wishtony <vishnutoanother@gmail.com>
2024-03-18 16:43:26 +0530

    new commit

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,18 @@
+import { SafeUser } from "@/app/types";
+
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
-const Navbar = () => {
-  return (
+interface NavbarProps {
+  currentUser?: SafeUser | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser,
+}) => {
+  return ( 
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div
         className="
@@ -12,15 +21,26 @@ const Navbar = () => {
         "
       >
       <Container>
-          <div className="flex 
-          flex-row
-           item-center 
-           justify-between gap-3 md:gap-0"><Logo/><Search/></div>
-        </Container>
-
-      </div>
+        <div 
+          className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+          <Search />
+          <UserMenu currentUser={currentUser} />
+        </div>
+      </Container>
     </div>
+    
+  </div>
   );
-};
+}
+
 
 export default Navbar;
